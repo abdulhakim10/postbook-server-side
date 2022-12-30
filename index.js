06646002f0cf1e34f0ad8201ec8a1770e1aa4bff
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-12-30 22:04:24 +0600

    create api to get and update users

--- index.js
+++ index.js
@@ -18,16 +18,69 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
 
     try{
+        const userCollection = client.db('postbookDb').collection('users');
         const postCollection = client.db('postbookDb').collection('posts');
         const commentCollection = client.db('postbookDb').collection('comments');
         const likeCollection = client.db('postbookDb').collection('likes');
 
+
+        // add users
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            const query = {email: user.email};
+            const exist = await userCollection.findOne(query);
+            // console.log(exist.email)
+            if(exist?.email === user.email){
+                return res.status(403).send('already exist');
+            }
+            else{
+                const result = await userCollection.insertOne(user);
+                res.send(result);
+            }
+            // console.log(result)
+        });
+
+
+        // update profile
+        app.put('/update', async(req, res) => {
+            const updatedProfile = req.body;
+            const email = req.query.email;
+            const filter = {email};
+            const options = {upsert: true};
+            const updatedDoc = {
+                $set: {
+                    displayName: updatedProfile.displayName,
+                    address: updatedProfile.address,
+                    university: updatedProfile.university,
+                    bio: updatedProfile.bio,
+                    photoURL: updatedProfile.photoURL
+                }
+                
+            };
+            const result = await userCollection.updateMany(filter, updatedDoc, options);
+            res.send(result);
+        });
+
+
+        // get user by email
+        app.get('/user', async(req, res) => {
+            const email = req.query.email;
+            const filter = {email}
+            const result = await userCollection.findOne(filter);
+            res.send(result);
+        });
+
+
+        // add post to db
         app.post('/posts', async (req, res) => {
             const post = req.body;
             const posts = await postCollection.insertOne(post);
             res.send(posts);
         });
 
+
+
+        // get post
         app.get('/posts', async(req, res) => {
             const query = {};
             const options = {
@@ -40,11 +93,14 @@ async function run(){
         });
 
 
-        // like send to db
+
+        // add like to db
         app.post('/likes', async(req, res) => {
             const likeInfo = req.body;
-            const result = await likeCollection.insertOne(likeInfo);
+            
+                const result = await likeCollection.insertOne(likeInfo);
             res.send(result);
+            
         });
 
 
@@ -57,7 +113,7 @@ async function run(){
         })
 
 
-        // comment send to db
+        // add comment to db
         app.post('/comments', async(req, res) => {
             const comment = req.body;
             const result = await commentCollection.insertOne(comment);
